7a5d9f878b7dfe0cb056f0ef737a90da92a116b6
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-05-29 19:00:23 +0200

    csv reader service

--- CsvConverterApp/Services/CsvReaderService.cs
+++ CsvConverterApp/Services/CsvReaderService.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Globalization;
+using CsvHelper;
 
-namespace CsvConverterApp.Services
+namespace CsvConverterApp.Services;
+
+public class CsvReaderService
 {
-    internal class CsvReaderService
+    public List<String> ReadHeader(string path)
     {
+        using var reader = new StreamReader(path);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        csv.Read();
+        csv.ReadHeader();
+
+        return csv.HeaderRecord?.ToList() ?? [];
+    }
+
+    public List<Dictionary<string, string>> ReadRows(string path)
+    {
+        using var reader = new StreamReader(path);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        var rows = new List<Dictionary<string, string>>();
+
+        csv.Read();
+        csv.ReadHeader();
+
+        while (csv.Read())
+        {
+            var row = new Dictionary<string, string>();
+            foreach (var header in csv.HeaderRecord)
+            {
+                row[header] = csv.GetField(header) ?? "";
+            }
+            rows.Add(row);
+        }
+
+        return rows;
     }
 }
