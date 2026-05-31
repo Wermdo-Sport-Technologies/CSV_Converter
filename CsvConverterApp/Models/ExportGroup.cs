d26680ad9fff97c119d5d2d46e90a82070f9bc55
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-05-31 19:30:17 +0200

    export to same file with excel in sheets

---
+++ CsvConverterApp/Models/ExportGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvConverterApp.Models;
+
+public class ExportGroup
+{
+    public string Name { get; set; } = "";
+    public List<Dictionary<string, string>> Rows { get; set; } = new();
+}
